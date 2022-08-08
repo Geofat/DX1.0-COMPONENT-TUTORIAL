2de7ddd053b00621a236270c29f72b7a7f7cf856
Geofat <geofat2015@gmail.com>
2022-08-08 13:56:04 +0100

    added javascript

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+// javascript using the variable method
+
+var seconddiv = document.getElementById('orangediv')
+// popping an alert when clicked
+seconddiv.addEventListener('click',function(){
+    alert('orange div is clicked')
+}) 
+
+// changing the element background
+seconddiv.style.background ="blue"
